241bd09b049b56dcec650ace3c345fbc276dface
victorgqueiroz <victorg.queiroz@hotmail.com>
2023-08-17 18:14:26 -0300

    testing animations text

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,32 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const listItems = document.querySelectorAll("navbar#left ul li");
-
-//   let selectedPartial = document.getElementById("about-page");
-
-//   listItems.forEach((item) => {
-//     item.addEventListener("click", (event) => {
-//       event.preventDefault();
-
-//       const clickedPartialId = item.classList[1];
-//       const clickedPartial = document.getElementById(`${clickedPartialId}-page`);
-
-//       selectedPartial.style.display = "none"; // Oculta a partial anterior
-//       clickedPartial.style.display = "block"; // Exibe a partial clicada
-
-//       selectedPartial = clickedPartial;
-
-//       listItems.forEach((li) => {
-//         li.classList.remove("active");
-//       });
-
-//       item.classList.add("active");
-//     });
-//   });
-
-//   // Adiciona a classe "active" à segunda <li> (li.about) quando a página for carregada
-//   const aboutLi = document.querySelector("navbar#left ul li.about");
-//   aboutLi.classList.add("active");
-// });
 
 document.addEventListener("DOMContentLoaded", () => {
   const listItems = document.querySelectorAll("navbar#left ul li");
@@ -60,4 +31,3 @@ document.addEventListener("DOMContentLoaded", () => {
   const aboutLi = document.querySelector("navbar#left ul li.about");
   aboutLi.classList.add("active");
 });
-
