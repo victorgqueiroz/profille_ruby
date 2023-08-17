d7e137e18119fa12aaf3c64eca0b2c7ac65bd650
victorgqueiroz <victorg.queiroz@hotmail.com>
2023-08-17 14:30:07 -0300

    scroll navbar fix

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,3 +1,33 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const listItems = document.querySelectorAll("navbar#left ul li");
+
+//   let selectedPartial = document.getElementById("about-page");
+
+//   listItems.forEach((item) => {
+//     item.addEventListener("click", (event) => {
+//       event.preventDefault();
+
+//       const clickedPartialId = item.classList[1];
+//       const clickedPartial = document.getElementById(`${clickedPartialId}-page`);
+
+//       selectedPartial.style.display = "none"; // Oculta a partial anterior
+//       clickedPartial.style.display = "block"; // Exibe a partial clicada
+
+//       selectedPartial = clickedPartial;
+
+//       listItems.forEach((li) => {
+//         li.classList.remove("active");
+//       });
+
+//       item.classList.add("active");
+//     });
+//   });
+
+//   // Adiciona a classe "active" à segunda <li> (li.about) quando a página for carregada
+//   const aboutLi = document.querySelector("navbar#left ul li.about");
+//   aboutLi.classList.add("active");
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
   const listItems = document.querySelectorAll("navbar#left ul li");
 
@@ -20,6 +50,9 @@ document.addEventListener("DOMContentLoaded", () => {
       });
 
       item.classList.add("active");
+
+      // Role a página até a seção correspondente
+      clickedPartial.scrollIntoView({ behavior: "smooth" });
     });
   });
 
@@ -27,3 +60,4 @@ document.addEventListener("DOMContentLoaded", () => {
   const aboutLi = document.querySelector("navbar#left ul li.about");
   aboutLi.classList.add("active");
 });
+
